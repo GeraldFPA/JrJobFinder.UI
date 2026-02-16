0429b8a5213adda2bbc349355b70b7aceb6eef11
GeraldFPA <GERALD.PALADINO@ucr.ac.cr>
2026-02-16 00:15:03 -0600

    se agregan los filtros de busqueda

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,23 +3,36 @@ import { getJobOffers } from "../services/JobOfferService";
 import type { JobOffer } from "../types/JobOffer";
 import { JobCard } from "../components/job/JobCard";
 import { Container } from "../components/layout/Container";
+import { FilterBar } from "../components/job/FilterBar";
 
 export function HomePage(){
     const [jobs, setJobs] = useState<JobOffer[]>([]);
     const [loading, setLoading] = useState(true);
+    
+    function loadJobs(filters?:any){
+        setLoading(true)
+        getJobOffers(filters)
+        .then(setJobs)
+        .finally(()=>setLoading(false))
+    }
 
     useEffect(()=>{
-        getJobOffers()
-        .then(setJobs)
-        .finally(() => setLoading(false));
+        loadJobs()
      },[])
-     if(loading) return <Container>Loading...</Container>
+
 
      return(
         <Container>
-            {jobs.map(job =>(
+            <FilterBar onSearch={loadJobs}/>
+            
+            {loading && <div>Loading...</div>}
+            
+            {!loading &&
+              jobs.map(job =>(
                 <JobCard key={job.id} job={job}/>
             ))}
+            
         </Container>
+     
     )
 }
--- src/services/JobOfferService.ts
+++ src/services/JobOfferService.ts
@@ -1,9 +1,33 @@
 import type { JobOffer } from "../types/JobOffer";
 
-export async function getJobOffers():Promise<JobOffer[]>{
-    const response = await fetch('/api/JobOffer ');
-    if(!response.ok){
-        throw new Error('Failed to fetch job offers', { cause: response.status });
-    }
-    return await response.json();
+interface Filters {
+  tecnology?: string;
+  level?: string;
+  isRemote?: boolean;
+  location?: string;
+}
+export async function getJobOffers(filters?: Filters): Promise<JobOffer[]> {
+  const filterParameters = new URLSearchParams();
+
+  if (filters?.tecnology) {
+    filterParameters.append("tecnology", filters.tecnology);
+  }
+  if (filters?.level) {
+    filterParameters.append("level", filters.level);
+  }
+  if (filters?.isRemote !== undefined) {
+    filterParameters.append("isRemote", String(filters.isRemote));
+  }
+  if (filters?.location) {
+    filterParameters.append("location", filters.location);
+  }
+
+  const query = filterParameters.toString();
+  const url = query ? `/api/JobOffer?${query}` : "/api/JobOffer";
+  const response = await fetch(url);
+  console.log(url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch job offers", { cause: response.status });
+  }
+  return await response.json();
 }
